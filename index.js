977f1583829ed8c3c0e6141c122f8e6cdaf18508
mononomo <hostdog01@naver.com>
2021-01-18 21:52:10 +0900

    server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var http = require("http");
 console.log("hello");
+var server = http.createServer(function (request, response) {
+    console.log("create");
+});
+server.listen(3000);
--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
+import * as http from "http";
+
 console.log("hello");
+
+const server = http.createServer((request, response) => {
+  console.log("create");
+});
+server.listen(3000);
